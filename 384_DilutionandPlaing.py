b8658abab7aec16e5f682a0035475ba648579b57
ljm176 <43517811+ljm176@users.noreply.github.com>
2021-03-25 15:40:45 +0100

    Fix plating bug.

--- 384_DilutionandPlaing.py
+++ 384_DilutionandPlaing.py
@@ -8,19 +8,14 @@ def run(protocol):
 
 	#Load Tips1
     tips20= [protocol.load_labware('opentrons_96_tiprack_20ul', '1')]
-
-
     p20Multi = protocol.load_instrument("p20_multi_gen2", "left", tip_racks=tips20)
-
-    agar_plate_type = "biorad_96_wellplate_200ul_pcr" #can be any 96 that isn't the same as dil plate
-    agar = protocol.load_labware(agar_plate_type, 3)
+    pl_384 = protocol.load_labware("echo_384_pp_standard", 2)  
+    agar = protocol.load_labware("biorad_96_wellplate_200ul_pcr", 3)    
     
-    pl_384 = protocol.load_labware("echo_384_pp_standard", 2)      
         
     def spot(dest, spot_vol):
         """Takes a diluted transformed culture and spots the defined volume onto agar 
         in a Nunc omnitray"""
-
         SAFE_HEIGHT = 15  
         spotting_dispense_rate=0.25 
         p20Multi.move_to(dest.top(SAFE_HEIGHT))
@@ -36,8 +31,6 @@ def run(protocol):
         spot(agar.wells()[p], 5)
         p20Multi.dispense(5, pl_384.wells()[w2])
 
-
-
     def dilute_plate(w, a):
         """dilutes in a square and plates"""
         p20Multi.pick_up_tip()
@@ -46,9 +39,8 @@ def run(protocol):
         mix_plate(w + 17, w + 1, a+16)
         p20Multi.mix(2, 20, pl_384.wells()[w+1])
         p20Multi.aspirate(5, pl_384.wells()[w+1])
-        spot(agar.wells()[24], 5)
+        spot(agar.wells()[a+24], 5)
         p20Multi.drop_tip()
-
 
     wells = [0, 128, 256]
     agar_locs = [0, 32, 64]
@@ -56,7 +48,6 @@ def run(protocol):
     for well, agar_loc in zip(wells, agar_locs):
         dilute_plate(well, agar_loc)
 
-        
     protocol.comment("Run Complete!")
         
         
